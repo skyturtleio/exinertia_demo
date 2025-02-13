c8b6526ed71dbf113989f9f619774adbfe453ce6
skyturtle <dev@skyturtle.io>
2025-02-13 10:16:58 -0800

    play around with controlled inputs

--- assets/js/pages/Welcome.tsx
+++ assets/js/pages/Welcome.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
 import { Head } from '@inertiajs/react'
+import { useState } from 'react';
 
 interface WelcomeProps {
   message: string;
   name: string;
 }
 
+const InputExample = () => {
+  const [text, setText] = useState('');
+
+  const handleChange = event => {
+    setText(event.target.value);
+  };
+
+  return (
+    <>
+      <p>{text}</p>
+      <input type='text' value={text} onChange={handleChange} />
+    </>
+  )
+}
 export default function WelcomePage({ message, name }: WelcomeProps) {
   return (
     <>
       <Head title="Welcome" />
       <h1 className="text-2xl font-bold underline">{message}</h1>
-      <p className="pt-4">Hello <span className="text-green-600 font-semibold">{name}</span>, welcome the Inertia.js demo app!</p>
+      <p className="pt-4">Hello <span className="text-green-600 font-semibold">{name}</span>, welcome to the Inertia.js demo app!</p>
+      <InputExample />
       <p className="pt-4">
         The code for this project can be found <a
           className="underline"
